2fba8d2f9671738e7340829ddbf608f7be08b9cb
“techfaithhubcontact1” <gulatiyajamte@gmail.com>
2023-09-28 12:31:10 +0530

    reactJs1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//1. Import Area
 
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import Aside from './components/Aside';
+import Section from './components/Section';
+import Footer from './components/Footer';
+
+ 
+//Function defination Area
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <main>
+        <Aside />
+        <Section />
+      </main>
+
+      <Footer />
+    </>
   );
 }
 
+// Export Area 
 export default App;
